f69f0aa18ee6b680c97a2a130c24befa5f751e26
Kacper Czochara <kacperczochara@gmail.com>
2020-04-26 14:34:05 +0200

    Better file validation

--- api/handlers/upload.go
+++ api/handlers/upload.go
@@ -2,13 +2,17 @@ package handlers
 
 import (
 	models "api/models"
+	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"regexp"
 	"time"
 
 	"github.com/gabriel-vasile/mimetype"
@@ -16,6 +20,12 @@ import (
 )
 
 const maxUploadSize = 15 * 1024 * 1024
+
+var allowedMimes = []string{
+	"audio/mpeg3", "audio/x-mpeg-3", "audio/mpeg",
+	"audio/wav", "audio/x-wav", "audio/wave",
+	"audio/ogg", "application/ogg",
+}
 
 // HandleUpload handles uploaded fles
 func HandleUpload(env *Env, w http.ResponseWriter, r *http.Request) error {
@@ -28,27 +38,16 @@ func HandleUpload(env *Env, w http.ResponseWriter, r *http.Request) error {
 	// parse and validate file and post parameters
 	file, fileHandler, err := r.FormFile("files")
 	if err != nil {
-		return HTTPError(http.StatusBadRequest, "invalid_file", err)
+		return HTTPError(http.StatusBadRequest, "invalid_post_params", err)
 	}
 	defer file.Close()
 	fileBytes, err := ioutil.ReadAll(file)
 	if err != nil {
-		return HTTPError(http.StatusBadRequest, "invalid_file", err)
+		return HTTPError(http.StatusBadRequest, "cant_read_file", err)
 	}
 
-	// check mime type
-	mimeFromRequest := http.DetectContentType(fileBytes)
-	log.Println("Detected file type:", mimeFromRequest)
-	switch mimeFromRequest {
-	case "audio/mpeg3", "audio/x-mpeg-3", "audio/mpeg":
-	case "audio/wav", "audio/x-wav", "audio/wave":
-	case "audio/ogg", "application/ogg":
-		break
-	default:
-		log.Println("Invalid file type:", mimeFromRequest)
-		return HTTPError(http.StatusBadRequest, "invalid_file_type", err)
-	}
-
+	// at this point we will write the file to the storage
+	// then we can check mime types
 	// get file extension and create new file path
 	fileExtension := mimetype.Detect(fileBytes).Extension()
 	if len(fileExtension) == 0 {
@@ -67,15 +66,34 @@ func HandleUpload(env *Env, w http.ResponseWriter, r *http.Request) error {
 		return HTTPError(http.StatusInternalServerError, "cant_write_file", err)
 	}
 
+	// validate file by mime type
+	mimeFromRequest := http.DetectContentType(fileBytes)
+	mimeFromFile, err := getMimeTypeFromFile(newPath)
+	if err != nil {
+		log.Printf("Can't read mime type from file: %s", err.Error())
+	}
+	// if mime type determined form both sources is invalid
+	if !allowedMimeType(mimeFromRequest) && !allowedMimeType(mimeFromFile) {
+		// if this fails, we have to remove the file from the storage
+		os.Remove(newPath)
+		return HTTPError(http.StatusBadRequest, "invalid_file_type_mime", nil)
+	}
+
 	// store data
-	env.DB.Write(models.DBAudio, fileName, models.Audio{
+	err = env.DB.Write(models.DBAudio, fileName, models.Audio{
 		File:         fileName + fileExtension,
 		Hash:         fileName,
 		Views:        0,
 		OriginalName: fileHandler.Filename,
+		RequestMIME:  mimeFromRequest,
+		FileMIME:     mimeFromFile,
 		CreatedAt:    time.Now().UTC().Format(time.RFC3339),
 		ViewedAt:     time.Now().UTC().Format(time.RFC3339),
 	})
+	if err != nil {
+		os.Remove(newPath)
+		return HTTPError(http.StatusInternalServerError, "db_error", err)
+	}
 
 	// write response
 	responseStruct := struct {
@@ -85,9 +103,50 @@ func HandleUpload(env *Env, w http.ResponseWriter, r *http.Request) error {
 	}
 	response, err := json.Marshal(responseStruct)
 	if err != nil {
+		env.DB.Delete(models.DBAudio, fileName)
+		os.Remove(newPath)
 		return HTTPError(http.StatusInternalServerError, "", err)
 	}
 	fmt.Fprint(w, string(response))
 
 	return nil
+}
+
+func allowedMimeType(mime string) bool {
+	for _, allowedMime := range allowedMimes {
+		if mime == allowedMime {
+			return true
+		}
+	}
+
+	return false
+}
+
+func getMimeTypeFromFile(path string) (string, error) {
+	out, err := execFileCmd(path)
+	if err != nil {
+		return "", err
+	}
+
+	r := regexp.MustCompile(`.*:\s+(\w+\/[\w-]+)`)
+	matches := r.FindAllStringSubmatch(out, -1)
+	if len(matches) < 1 || len(matches[0]) < 2 {
+		return "", errors.New("Could't parse `file` output: " + out)
+	}
+
+	return matches[0][1], nil
+}
+
+func execFileCmd(path string) (string, error) {
+	cmd := exec.Command("file", "--mime-type", path)
+
+	var out bytes.Buffer
+	cmd.Stdout = &out
+
+	err := cmd.Run()
+	if err != nil {
+		return "", err
+	}
+
+	return out.String(), nil
 }
--- api/models/audio.go
+++ api/models/audio.go
@@ -1,12 +1,16 @@
 package models
 
+// DBAudio is database name for Audio model
 const DBAudio = "audio"
 
+// Audio represents the JSON data structure for audio files
 type Audio struct {
 	File         string `json:"file"`
 	Hash         string `json:"hash"`
 	Views        int    `json:"views"`
 	OriginalName string `json:"original_name"`
+	RequestMIME  string `json:"request_mime"`
+	FileMIME     string `json:"file_mime"`
 	CreatedAt    string `json:"created_at"`
 	ViewedAt     string `json:"viewed_at"`
 }
